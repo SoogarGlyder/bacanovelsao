0ee07496c56f17bccb729337d23fcdaef782ad3c
Baskoro Cahhyo Heri Nugroho <baskorocahhyo@Baskoros-MacBook-Air.local>
2025-11-20 12:56:07 +0700

    feat: initial mern react frontend

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header.jsx';
+
+function App() {
+  const [pageSerie, setPageSerie] = useState(null); 
+  const [dropdownSerie, setDropdownSerie] = useState(null); 
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  useEffect(() => {
+    if (isListOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isListOpen]);
+
+  const activeSerie = isListOpen ? dropdownSerie : pageSerie;
+
+  return (
+    <>
+      <Header 
+        activeSerie={activeSerie}
+        isListOpen={isListOpen}
+        setDropdownSerie={setDropdownSerie}
+        setIsListOpen={setIsListOpen}
+        dropdownSerie={dropdownSerie}
+      />
+      
+      <main style={{ marginTop: 'var(--total-header-height)' }}>
+        <Outlet context={{ setPageSerie, setDropdownSerie, setIsListOpen }} />
+      </main>
+    </>
+  );
+}
+export default App;
