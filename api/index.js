fa957a5b0b6d89093ddf0e3f4416c769bb2751fb
Baskoro Cahhyo Heri Nugroho <baskorocahhyo@Baskoros-MacBook-Air.local>
2025-11-20 22:50:09 +0700

    fix: router

--- api/index.js
+++ api/index.js
@@ -8,11 +8,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/novels', novelRoutes);
+app.use('/api/chapters', chapterRoutes);
 
-app.use('/novels', novelRoutes);
-app.use('/chapters', chapterRoutes);
-
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('API is running via Vercel Serverless Function.');
 });
 export default async (req, res) => {
