2c68e1aa9f591fd033061d31b2982056db2c4663
Baskoro Cahhyo Heri Nugroho <baskorocahhyo@Baskoros-MacBook-Air.local>
2025-11-24 07:52:22 +0700

    slug

--- src/pages/ChapterReadPage.jsx
+++ src/pages/ChapterReadPage.jsx
@@ -165,17 +165,12 @@ return (
       </main>
 
     <aside className={styles.rightSidebar}>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4365395677457990"
-             crossorigin="anonymous"></script>
-        <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-4365395677457990"
-        data-ad-slot="4896743654"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+      <ins className="adsbygoogle"
+           style={{display: 'block'}}
+           data-ad-client="ca-pub-4365395677457990"
+           data-ad-slot="4896743654"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
     </aside>
 
   </div>
